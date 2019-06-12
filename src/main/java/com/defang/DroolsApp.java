2d7deb29f03206986cc70a5ad235318afeccedd6
Yincai.Xiong <Yingcai.Xiong@defangchain.com>
2019-06-12 10:50:46 +0800

    drools

--- src/main/java/com/defang/DroolsApp.java
+++ src/main/java/com/defang/DroolsApp.java
@@ -1,2 +1,22 @@
 package com.defang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableTransactionManagement
+@ComponentScan({ "com.defang" })
+@EnableSwagger2
+@EnableScheduling
+public class DroolsApp {
+	public static void main(String[] args) {
+		new SpringApplication(DroolsApp.class).run(args);
+	}
+
+}
---
+++ src/test/java/com/defang/drools/ScoreCardTest.java
@@ -0,0 +1,63 @@
+package com.defang.drools;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.KieServices;
+import org.kie.api.command.Command;
+import org.kie.api.command.KieCommands;
+import org.kie.api.runtime.ExecutionResults;
+import org.kie.server.api.marshalling.MarshallingFormat;
+import org.kie.server.api.model.ServiceResponse;
+import org.kie.server.client.KieServicesClient;
+import org.kie.server.client.KieServicesConfiguration;
+import org.kie.server.client.KieServicesFactory;
+import org.kie.server.client.RuleServicesClient;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.defang.scorecard.entity.Applicant;
+
+
+
+@RunWith(SpringRunner.class)
+public class ScoreCardTest {
+   
+    private static final String SERVER_URL = "http://192.168.80.236:8180/kie-server/services/rest/server";
+	private static final String USERNAME = "kieserver";
+	private static final String PASSWORD = "kieserver1!";
+	private static final String KIE_CONTAINER_ID = "scorecard_1.0.0-SNAPSHOT";
+	private static final String KIE_SESSION_ID = "scorecard";
+    
+	  @Test
+	  public void testScoreCard() {
+		// KisService 配置信息设置
+	        KieServicesConfiguration kieServicesConfiguration =
+	                KieServicesFactory.newRestConfiguration(SERVER_URL, USERNAME, PASSWORD, 10000L);
+	        kieServicesConfiguration.setMarshallingFormat(MarshallingFormat.JSON);
+
+	        // 创建规则服务客户端
+	        KieServicesClient kieServicesClient = KieServicesFactory.newKieServicesClient(kieServicesConfiguration);
+	        RuleServicesClient ruleServicesClient = kieServicesClient.getServicesClient(RuleServicesClient.class);
+
+	        // 规则输入条件  Applicant( age,sex,maritalStatus,degree,score)
+	        //Applicant applicant = new Applicant(23,"1","1","1",0);
+	        Applicant applicant = new Applicant(23,"0","0","0",0);
+
+	        // 命令定义，包含插入数据，执行规则
+	        KieCommands kieCommands = KieServices.Factory.get().getCommands();
+	        List<Command<?>> commands = new LinkedList<>();
+	        commands.add(kieCommands.newInsert(applicant, "applicant"));
+	        //commands.add(kieCommands.newGetFactHandle(new RuleNameStartsWithAgendaFilter("scorecard")));
+	        commands.add(kieCommands.newFireAllRules());
+	        ServiceResponse<ExecutionResults> results = ruleServicesClient.executeCommandsWithResults(KIE_CONTAINER_ID,
+	                kieCommands.newBatchExecution(commands, KIE_SESSION_ID));
+
+	         // 返回值读取
+	        Applicant newApplicant = (Applicant) results.getResult().getValue("applicant");
+		    System.out.println(newApplicant.toString());
+	  }
+	
+
+}
