7652a2f73781a10b4318bb1c5615ef7b3566b07b
Yincai.Xiong <Yingcai.Xiong@defangchain.com>
2019-06-12 09:41:19 +0800

    test

--- src/main/java/com/defang/DroolsApp.java
+++ src/main/java/com/defang/DroolsApp.java
@@ -1,16 +1,2 @@
 package com.defang;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-/**
- * @Description
- * @author mali
- */
-@ComponentScan({ "com.defang" })
-public class DroolsApp {
-	public static void main(String[] args) {
-		new SpringApplication(DroolsApp.class).run(args);
-	}
-
-}
